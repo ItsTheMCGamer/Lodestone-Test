015e04164f420a3d28e6f65bf8cb9bef7520d0fb
ItsTheMCGamer <novapanther1@gmail.com>
2024-02-09 19:13:20 +0100

    Fixing buffer problems

--- src/main/java/net/mcgamer/lodestone_test/item/custom/ParticleStickItem.java
+++ src/main/java/net/mcgamer/lodestone_test/item/custom/ParticleStickItem.java
@@ -1,5 +1,6 @@
 package net.mcgamer.lodestone_test.item.custom;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.mcgamer.lodestone_test.LodestoneTest;
 import net.minecraft.client.Minecraft;
@@ -17,6 +18,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import org.joml.Matrix4f;
 import team.lodestar.lodestone.handlers.RenderHandler;
 import team.lodestar.lodestone.registry.client.LodestoneRenderTypeRegistry;
@@ -33,30 +35,25 @@ import java.awt.*;
 import static com.mojang.blaze3d.vertex.VertexFormat.Mode.TRIANGLES;
 
 public class ParticleStickItem extends Item {
-    public static final RenderType renderType = LodestoneRenderTypeRegistry.TEXTURE.applyWithModifier(new ResourceLocation(LodestoneTest.MOD_ID,
-            "textures/misc/beam.png"), b -> b.replaceVertexFormat(TRIANGLES));
-    static VertexConsumer vertexConsumer = RenderHandler.DELAYED_RENDER.getBuffer(renderType);
     public ParticleStickItem(Properties pProperties) {
         super(pProperties);
     }
 
-    public InteractionResult useOn(UseOnContext pContext, RenderLevelStageEvent event) {
-        if(!pContext.getLevel().isClientSide()) {
-            BlockState state = pContext.getLevel().getBlockState(pContext.getClickedPos());
-            if(isGrassBlock(state)) {
-                renderScreen(event);
-            }
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand,
+                                                  RenderLevelStageEvent event, Color color) {
+        ItemStack itemstack = pPlayer.getItemInHand(pUsedHand);
+        if (pLevel.isClientSide() && pPlayer.getUsedItemHand().equals(pUsedHand)) {
+            renderSphere(event);
         }
-
-    return InteractionResult.SUCCESS;
+        return InteractionResultHolder.success(itemstack);
     }
 
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
-        if(usedHand.equals(InteractionHand.MAIN_HAND))
-            spawnParticle(level, player, Color.CYAN, Color.blue);
-        return super.use(level, player, usedHand);
-    }
+    //@Override
+    //public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+    //if(usedHand.equals(InteractionHand.MAIN_HAND))
+    //spawnParticle(level, player, Color.CYAN, Color.blue);
+    //return super.use(level, player, usedHand);
+    //}
 
     private static void spawnParticle(Level level, Player player, Color color, Color endColor) {
         if (level.isClientSide) {
@@ -78,14 +75,30 @@ public class ParticleStickItem extends Item {
 
     //RenderHandler.DELAYED_RENDER.getBuffer(renderType);
 
-    private static void renderScreen(RenderLevelStageEvent event) {
+    private static void renderBeam(RenderLevelStageEvent event, Color color) {
         Matrix4f matrix4f = event.getPoseStack().last().pose();
         Vec3 posStart = Minecraft.getInstance().getCameraEntity().getEyePosition();
         Vec3 posEnd = new Vec3(posStart.x + 20, posStart.y, posStart.z + 20);
-        VFXBuilders.WorldVFXBuilder builder = VFXBuilders.createWorld().setPosColorTexLightmapDefaultFormat()
-                .renderBeam(vertexConsumer, matrix4f, posStart, posEnd,
-                20);
+        RenderType renderType = LodestoneRenderTypeRegistry.TEXTURE.applyWithModifier(new ResourceLocation(LodestoneTest.MOD_ID,
+                "textures/misc/beam.png"), b -> b.replaceVertexFormat(TRIANGLES));
+        VertexConsumer vertexConsumer = RenderHandler.DELAYED_RENDER.getBuffer(renderType);
+        VFXBuilders.WorldVFXBuilder builder = VFXBuilders.createWorld().setPosColorTexLightmapDefaultFormat();
+        builder.renderBeam(vertexConsumer, matrix4f, posStart, posEnd, 20)
+                .setColor(color)
+                .setLight(30);
     }
+
+    private static void renderSphere(RenderLevelStageEvent event) {
+        PoseStack poseStack = event.getPoseStack();
+        RenderType renderType = LodestoneRenderTypeRegistry.TEXTURE.applyWithModifier(new ResourceLocation(LodestoneTest.MOD_ID,
+                "textures/misc/sphere.png"), b -> b.replaceVertexFormat(TRIANGLES));
+        VertexConsumer vertexConsumer = RenderHandler.DELAYED_RENDER.getBuffer(renderType);
+        poseStack.pushPose();
+        VFXBuilders.WorldVFXBuilder builder = VFXBuilders.createWorld().setPosColorTexLightmapDefaultFormat();
+        builder.renderSphere(vertexConsumer, poseStack, 3, 50, 50);
+        poseStack.popPose();
+    }
+
     private boolean isGrassBlock(BlockState state) {
         return state.is(Blocks.GRASS_BLOCK);
     }
